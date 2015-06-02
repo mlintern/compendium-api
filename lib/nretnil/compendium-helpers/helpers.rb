043b55169fa56c36783304db15dbfb6c9cea3748
Mark Lintern <mark.lintern@oracle.com>
2015-06-02 10:29:30 -0400

    0.15.0
    fix helper.network_id

--- lib/nretnil/compendium-helpers/helpers.rb
+++ lib/nretnil/compendium-helpers/helpers.rb
@@ -30,9 +30,8 @@ module Nretnil
       end
 
       def network_id
-        response = @session.content.list({ :Page => '1', :Count => '1', :Status => ["approved"].to_json })
-        response = @session.content.get(response["content"][0]["id"])
-        network_id = response["network_id"]
+        response = @session.content_type.list
+        network_id = response[0]["network_id"]
       end
 
       def pub_ids
