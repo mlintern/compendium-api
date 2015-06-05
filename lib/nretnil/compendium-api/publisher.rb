ff59c84b90fcc2618cc1b2ccf921773f91d910e6
Mark Lintern <mark.lintern@oracle.com>
2015-06-05 13:48:06 -0400

    0.15.2
    pulisher add fix

--- lib/nretnil/compendium-api/publisher.rb
+++ lib/nretnil/compendium-api/publisher.rb
@@ -28,7 +28,7 @@ module Nretnil
 
       def add(name,hostname,options={})
         body = { :publisher_name => name, :publisher_title => name, :hostname => hostname, :uri_base => "/", :publisher_type => "compendium", :page_title_format => "SHORT" }.merge(options)
-        response = @session.post( '/api/publishers', body.to_json )
+        response = @session.post( '/api/publishers', body.to_json, {}, { "Content-Type" => "application/x-json" } )
       end
 
       def required_params
