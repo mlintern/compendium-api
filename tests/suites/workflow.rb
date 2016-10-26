fc9ff58cdc6c1bf965a213af787a58da936aaf0c
Mark Lintern <mark.lintern@oracle.com>
2016-10-26 11:56:46 -0400

    stubbed out final suites

---
+++ tests/suites/workflow.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# frozen_string_literal: true
+require File.expand_path(File.dirname(__FILE__) + '/../unit_test')
+
+# Workflow Unit Tests
+class Workflow < UnitTests
+  def test_workflow_list
+    req = @admin.workflow.list
+    assert req.code == 200
+  end
+end
