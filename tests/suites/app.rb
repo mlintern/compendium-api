0cbcca2f8fd9135e80da01e9300180604463f9b4
Mark Lintern <mark.lintern@oracle.com>
2016-10-26 11:06:47 -0400

    fixed endpoint and updated documentation

--- tests/suites/app.rb
+++ tests/suites/app.rb
@@ -6,14 +6,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../unit_test')
 class App < UnitTests
   def test_app_list
     req = @admin.app.list
-    puts req.inspect
-    assert req.code == 200
-  end
-
-  def test_app_get
-    puts list = @admin.app.list
-    assert list.code == 200
-    req = @admin.app.get(list[0]['id'])
     assert req.code == 200
   end
 end
